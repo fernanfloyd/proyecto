e0690c95f0d9ba526a108eabd1a2dec945d9bcd7
Fernando Pérez <fperezllop@gmail.com>
2013-06-05 17:20:27 +0200

    ARREGLADO FALLO DISEÑO EN CONFIRMAR COMPRA
    - Lo que ha dicho David por Whatsapp que salia mal lo de confirmar
    compra, con mucho espacio en blanco, ya esta arreglado

--- confirmarCompra.php
+++ confirmarCompra.php
@@ -101,7 +101,7 @@
 				<hr />
 				<div id="cliente">
 					<?php
-						echo "<div class='cajaMostrar'>";
+						echo "<div class='cajaMostrar' style='height:auto;'>";
 							echo "<div class='cajaDatos'>";
 							$datos=obtenerTodoUsuario($idU);
 							if($datos['n_tarjeta']==""){
